be81e649632c5c0a8f482daf14ea8ee35fe38801
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-07-29 16:02:11 +0300

    Create silme.php
    Veritabanı blog silme delete from

---
+++ Blog-Yönetim-Sistemi/silme.php
@@ -0,0 +1,17 @@
+<?php
+include 'baglan.php';
+$baglanti = new mysqli($host, $kullanici, $sifre, $veritabani);
+if ($baglanti->connect_error) {
+	die("Bağlantı Hatası: " . $baglanti->connect_error);
+}
+$sql = "DELETE FROM yazilar WHERE id=".$_REQUEST['ID']."";
+$sonuc = $baglanti->query($sql);
+if ($sonuc === true) {
+	echo "Successfull (Yazı veritabanından başarılı silindi.)";
+    echo "<br><br><a href='index.php'>HOMEPAGE (ANASAYFA)</a>";
+} else {
+    echo "Unsuccessfull (İlgili yazı veritabanından silinemedi! Lütfen tekrar deneyiniz.)";
+    echo "<br><br><a href='sil.php?id".$_REQUEST['ID'].">BACK (GERİ)</a>";
+}
+$baglanti->close();
+?>
