2f7412f084a49a0a571b80483a813e3a6f05ad7f
Neslihan ATALAY <118984486+Neslihan-ATALAY@users.noreply.github.com>
2025-07-29 15:50:14 +0300

    Create detay.php
    Blog detay

---
+++ Blog-Yönetim-Sistemi/detay.php
@@ -0,0 +1,32 @@
+<?php
+include 'baglan.php';
+$baglanti = new mysqli($host, $kullanici, $sifre, $veritabani);
+if ($baglanti->connect_error) {
+	die("Bağlantı Hatası: " . $baglanti->connect_error);
+}
+$sql = "SELECT * FROM yazilar WHERE id=".$_REQUEST['ID']."";
+$sonuc = $baglanti->query($sql);
+if ($sonuc->num_rows > 0) {
+	while($row = $sonuc->fetch_assoc()) {
+		$tarihvesaat[] = explode(" ", $row["tarih"]);	
+		foreach($tarihvesaat as [$tar, $saa]) {
+			$tarih = $tar;
+			$saat = $saa;
+		}
+		// Detay formu
+		echo '
+		<form>
+			ID:<br><input type="label" name="id" value="'.$row["id"].'" disabled/><br><br>
+			HEADER (BAŞLIK):<br><input type="label" style="width:750px" name="baslik" value="'.$row["baslik"].'" disabled/><br><br>
+			CONTENT (İÇERİK):<br><textarea cols="100" rows="25" name="icerik" disabled>'.$row["icerik"].'</textarea><br><br>
+			DATE/TIME (TARİH/SAAT):<br><input type="date" style="width:200px" name="tarih" value="'.$tarih.'" disabled/>  
+			<input type="time" style="width:100px" name="saat" value="'.$saat.'" disabled/><br>
+			<br><a href="index.php">HOMEPAGE (ANASAYFA)</a>
+		</form>';
+	}
+} else {
+	  echo "No record! (İlgili yazı veritabanında bulunamadı! Lütfen tekrar deneyiniz.)";
+    echo "<br><br><a href='index.php'>HOMEPAGE (ANASAYFA)</a>";
+}
+$baglanti->close();
+?>
